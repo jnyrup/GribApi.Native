57765d52411f99385662aac95e47224be17d00af
jnyrup <jnyrup@gmail.com>
2019-08-03 15:29:28 +0200

    packagin

--- build/win32/build_nuget.cmd
+++ build/win32/build_nuget.cmd
@@ -5,15 +5,15 @@ if "%1"=="" SET _VERSION=0.0.0-beta
 
 RD %~dp0..\..\nuget.package\content /S /Q
 
-xcopy %~dp0..\..\bin\Release\Grib.Api\lib\win\x64\Grib.Api.Native.dll %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x64\Release /S /Y /I /Q
-xcopy %~dp0..\..\bin\Release\Grib.Api\lib\win\x64\Grib.Api.Native.pdb %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x64\Release /S /Y /I /Q
-xcopy %~dp0..\..\bin\Debug\Grib.Api\lib\win\x64\Grib.Api.Native.dll %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x64\Debug /S /Y /I /Q
-xcopy %~dp0..\..\bin\Debug\Grib.Api\lib\win\x64\Grib.Api.Native.pdb %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x64\Debug /S /Y /I /Q
+xcopy %~dp0..\..\bin\Release\Grib.Api\lib\win\x64\Grib.Api.Native.dll %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x64\Release\ /S /Y /Q
+xcopy %~dp0..\..\bin\Release\Grib.Api\lib\win\x64\Grib.Api.Native.pdb %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x64\Release\ /S /Y /Q
+xcopy %~dp0..\..\bin\Debug\Grib.Api\lib\win\x64\Grib.Api.Native.dll %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x64\Debug\ /S /Y /Q
+xcopy %~dp0..\..\bin\Debug\Grib.Api\lib\win\x64\Grib.Api.Native.pdb %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x64\Debug\ /S /Y /Q
 
-xcopy %~dp0..\..\bin\Release\Grib.Api\lib\win\x86\Grib.Api.Native.dll %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x86\Release /S /Y /I /Q
-xcopy %~dp0..\..\bin\Release\Grib.Api\lib\win\x86\Grib.Api.Native.pdb %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x86\Release /S /Y /I /Q
-xcopy %~dp0..\..\bin\Debug\Grib.Api\lib\win\x86\Grib.Api.Native.dll %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x86\Debug /S /Y /I /Q
-xcopy %~dp0..\..\bin\Debug\Grib.Api\lib\win\x86\Grib.Api.Native.pdb %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x86\Debug /S /Y /I /Q
+xcopy %~dp0..\..\bin\Release\Grib.Api\lib\win\x86\Grib.Api.Native.dll %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x86\Release\ /S /Y /Q
+xcopy %~dp0..\..\bin\Release\Grib.Api\lib\win\x86\Grib.Api.Native.pdb %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x86\Release\ /S /Y /Q
+xcopy %~dp0..\..\bin\Debug\Grib.Api\lib\win\x86\Grib.Api.Native.dll %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x86\Debug\ /S /Y /Q
+xcopy %~dp0..\..\bin\Debug\Grib.Api\lib\win\x86\Grib.Api.Native.pdb %~dp0..\..\nuget.package\content\net45\Grib.Api\lib\win\x86\Debug\ /S /Y /Q
 
 xcopy %~dp0..\..\GribApi.XP\grib_api\definitions %~dp0..\..\nuget.package\content\net45\Grib.Api\definitions\ /S /d /I /Q /Y
 xcopy %~dp0..\..\GribApi.XP\grib_api\samples %~dp0..\..\nuget.package\content\net45\Grib.Api\samples\ /S /d /I /Q /Y
